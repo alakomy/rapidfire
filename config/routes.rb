876bafd2f0845c15d877ff146f036c2cc7d82561
eventlinkdigital <58052352+eventlinkdigital@users.noreply.github.com>
2019-12-05 14:18:46 -0600

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rapidfire::Engine.routes.draw do
 
     resources :questions
     
-    match '/surveys/:api_id/attempts(.:format)' => 'rapidfire/attempts#create', via: [:post], as: :survey_attempts
-    match '/surveys/:api_id/attempts/new(.:format)' => 'rapidfire/attempts#new', via: [:get], as: :new_survey_attempt
-    match '/surveys/:api_id/attempts/:id/edit(.:format)' => 'rapidfire/attempts#edit', via: [:get], as: :edit_survey_attempt
-    match '/survey_attempt' => 'rapidfire/attempts#show', via: [:get], as: :survey_attempt
+    match 'surveys/:api_id/attempts(.:format)' => 'rapidfire/attempts#create', via: [:post], as: :survey_attempts
+    match 'surveys/:api_id/attempts/new(.:format)' => 'rapidfire/attempts#new', via: [:get], as: :new_survey_attempt
+    match 'surveys/:api_id/attempts/:id/edit(.:format)' => 'rapidfire/attempts#edit', via: [:get], as: :edit_survey_attempt
+    match 'survey_attempt' => 'rapidfire/attempts#show', via: [:get], as: :survey_attempt
     
     
   end
