e470c519519189b910c65b4f1a4a65b8fb042f7a
eventlinkdigital <58052352+eventlinkdigital@users.noreply.github.com>
2019-12-05 18:53:56 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,17 @@ Rapidfire::Engine.routes.draw do
 
     resources :questions
     
-    match 'surveys/:api_id/attempts(.:format)' => 'rapidfire/attempts#create', via: [:post], as: :survey_attempts
-    match 'surveys/:api_id/attempts/new(.:format)' => 'rapidfire/attempts#new', via: [:get], as: :new_survey_attempt
-    match 'surveys/:api_id/attempts/:id/edit(.:format)' => 'rapidfire/attempts#edit', via: [:get], as: :edit_survey_attempt
-    match 'survey_attempt' => 'rapidfire/attempts#show', via: [:get], as: :survey_attempt
     
+    
+    
+  end
+  
+  scope '/surveys' do
+    
+    match '/:api_id/attempts(.:format)' => 'rapidfire/attempts#create', via: [:post], as: :survey_attempts
+    match '/:api_id/attempts/new(.:format)' => 'rapidfire/attempts#new', via: [:get], as: :new_survey_attempt
+    match '/:api_id/attempts/:id/edit(.:format)' => 'rapidfire/attempts#edit', via: [:get], as: :edit_survey_attempt
+    match 'survey_attempt' => 'rapidfire/attempts#show', via: [:get], as: :survey_attempt
     
   end
 
