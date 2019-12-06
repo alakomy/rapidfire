9d61d655df8db65e511e188c19f02d01298ea71a
eventlinkdigital <58052352+eventlinkdigital@users.noreply.github.com>
2019-12-05 21:07:37 -0600

    Update survey.rb

--- app/models/rapidfire/survey.rb
+++ app/models/rapidfire/survey.rb
@@ -1,5 +1,10 @@
 module Rapidfire
   class Survey < ActiveRecord::Base
+    
+    def to_param  # overridden
+      api_id
+    end
+    
     has_many  :attempts
     has_many  :questions
     has_many :attempts
