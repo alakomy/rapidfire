ca36ba75617cd52ff220f80718ce5d62a0c34cce
eventlinkdigital <58052352+eventlinkdigital@users.noreply.github.com>
2020-01-23 09:37:21 -0600

    adding total count

--- app/services/rapidfire/question_result.rb
+++ app/services/rapidfire/question_result.rb
@@ -2,7 +2,7 @@ module Rapidfire
   class QuestionResult < Rapidfire::BaseService
     include ActiveModel::Serialization
 
-    attr_accessor :question, :results
+    attr_accessor :question, :results, :total_count
 
     def active_model_serializer
       Rapidfire::QuestionResultSerializer
